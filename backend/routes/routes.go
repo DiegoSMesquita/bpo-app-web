53abf2706b2b3278bac2cbcfbeb0cc3688d055c0
Diego Mesquita <48533869+DiegoSMesquita@users.noreply.github.com>
2025-08-01 11:16:17 -0300

    v

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,12 +1,14 @@
 package routes
 
 import (
+	"github.com/DiegoSMesquita/bpo-app-web-main/config"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-
+	// Aplica o middleware de CORS
+	r.Use(config.SetupCORS())
 	AuthRoutes(r)
 	EmpresaRoutes(r)
 
